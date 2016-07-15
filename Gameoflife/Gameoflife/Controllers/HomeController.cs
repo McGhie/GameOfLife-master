e396d9c5325f9cc8387cb5f2d7beccd0f5484efd
Argyll <argyllmcghie@gmail.com>
2016-07-15 10:59:23 +1000

    First upload

---
+++ Gameoflife/Gameoflife/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Gameoflife.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Templates()
+        {
+            ViewBag.Message = "Available Templates";
+
+            return View();
+        }
+
+        public ActionResult Active()
+        {
+            ViewBag.Message = "Your Active Games";
+
+            return View();
+        }
+    }
+}
