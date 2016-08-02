bd1216cd7b53426e585b775fa700d30a411e132a
SteveMaynard <s3397906@student.rmit.edu.au>
2016-08-02 21:16:33 +1000

    Add files via upload

---
+++ DataBaseMetadata.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Gameoflife.Models
+{
+
+   
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "The email address is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "The password is required")]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+      
+        [Required(ErrorMessage = "The first name is required")]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 2)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "The last name is required")]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength =2)]
+        [Display(Name = "Last Name")]
+        public string LastName{ get; set; }
+
+
+
+
+    }
+
+    namespace Models 
+    {
+        [MetadataType(typeof(RegisterViewModel))]
+        
+        public partial class User
+        {
+            
+        }
+
+    
+
+    }
+
+
+
+ 
+}
---
+++ UsersController.cs
@@ -0,0 +1,103 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using CryptSharp;
+using Gameoflife.Models;
+
+namespace Gameoflife.Controllers
+{
+    public class UsersController : Controller
+    {
+        private GameOfLifeDataEntities1 db = new GameOfLifeDataEntities1();
+
+
+        // GET: Users/Create
+        [AllowAnonymous]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        // POST: Users/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register([Bind(Include = "Email,Password,FirstName,LastName")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                User exists = db.Users.FirstOrDefault(u => u.Email == user.Email || (u.FirstName==user.FirstName && u.LastName==user.LastName));
+                if (exists != null)
+                {
+                    ViewBag.RegisterErrorMessage = "User already exists";
+                }
+                else
+                {
+                    user.IsAdmin = false;
+                    user.Password = Crypter.Blowfish.Crypt(user.Password);
+                    db.Users.Add(user);
+                    db.SaveChanges();
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("Login")]
+        public ActionResult Login([Bind(Include = "Email,Password")] User user)
+        {
+            using (var database = new GameOfLifeDataEntities1())
+
+            {
+                User login = database.Users.FirstOrDefault(u => u.Email == user.Email);
+
+                if (login != null)
+                {
+                    bool matches = Crypter.CheckPassword(user.Password ,login.Password );
+                    if (matches != true)
+                    {
+                        ViewBag.LoginErrorMessage = "Fail To log in";
+                    }
+                    else
+                    {
+                        Session["User"] = user;
+                        return RedirectToAction("Index", "Home");
+                    }
+                    
+                    
+                }
+
+             
+            }
+            ViewBag.LoginErrorMessage = "Fail To log in";
+
+            return View(user);
+        }
+
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            return RedirectToAction("Index", "Home");
+
+
+
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
