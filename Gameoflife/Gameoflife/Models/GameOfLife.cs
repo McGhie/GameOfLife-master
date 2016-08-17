bca35c89e3662794e502abb6bed85427d0a63beb
Argyll <argyllmcghie@gmail.com>
2016-08-17 10:04:27 +1000

    fixed the gameof life functions
    There where a couple of issues that I did not see until I tried a simple
    blinker.
    Template was copying over itself in reverse on start up, Removed code.
    
    The take turn was not creating a new cell [][] to copy into, thiss means
    that it changed the result for the next cell being tested.
    
    The rules where not checking if the cell was already alive or dead
    before doing the count.... There are two rules, check out assignment 1
    again.

--- Gameoflife/Gameoflife/Models/GameOfLife.cs
+++ Gameoflife/Gameoflife/Models/GameOfLife.cs
@@ -53,25 +53,7 @@ namespace GameOfLife
                 }
             }
             Cells = tempCells;
-            Counter = 0;
-            for (int h = 0; h <= Height - 1; h++)
-            {
-                tempCells[h] = new Cell[Width];
-                for (int w = 0; w <= Width - 1; w++)
-                {
-                    if (cellArray[Counter].ToString().Equals("0"))
-                    {
-
-                        tempCells[h][w] = Cell.Dead;
-                    }
-                    else
-                    {
-                        tempCells[h][w] = Cell.Alive;
-
-                    }
-                    Counter++;
-                }
-            }
+     
         }
 
 
@@ -88,25 +70,55 @@ namespace GameOfLife
         {
             
             var tempCells = Cells;
+            //We need to create a new instance of a cell to copy into
+            //we can not just copy into the current one as it will change the result for the next cell being looked at.
+            //we need to do this twice once for [height] and again for [width]-this is in the inner loop.
+            Cell[][] CellClone = new Cell[Height][];
+            
             for (int h = 0; h <= Height - 1; h++)
             {
-               for (int w = 0; w <= Width - 1; w++)
+                CellClone[h] = new Cell[Width];
+                for (int w = 0; w <= Width - 1; w++)
                 {
-                    int aliveCount = CountLivingNeighbours(h, w);
 
-                    if (aliveCount < 2 || aliveCount > 3)
+                    //We need to check if the cell is dead or alive as there are different rules for each
+
+                    if (tempCells[h][w] == Cell.Alive) //Alive Cell Rule can have 2 or 3 living neighbours
                     {
-                        tempCells[h][w] = Cell.Dead;
-                    }
-                    else if(aliveCount == 3)
-                    {
-                        tempCells[h][w] = Cell.Alive;
+                        
+                        int aliveCount = CountLivingNeighbours(h, w);
+
+                        if (aliveCount < 2 || aliveCount > 3)
+                        {
+                            CellClone[h][w] = Cell.Dead;
+                        }
+                        else 
+                        {
+                            CellClone[h][w] = Cell.Alive;
+                        }
                     }
 
+                    if (tempCells[h][w] == Cell.Dead) //Dead Cell Rule can only have 3 living neighbours
+                    {
+
+                        int aliveCount = CountLivingNeighbours(h, w);
+
+                         if (aliveCount == 3)
+                        {
+                            CellClone[h][w] = Cell.Alive;
+                        }
+
+                        else
+                        {
+                            CellClone[h][w] = Cell.Dead;
+                        }
+                       
+                    }
+                    
                 }
                 
             }
-            Cells = tempCells;
+            Cells = CellClone;
 
            
             
