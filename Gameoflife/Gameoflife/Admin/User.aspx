771aacd7e0defcb696f1f82075d6df0b6accbbfb
Jerome Goldstein <jeromegold@gmail.com>
2016-08-19 15:00:14 +1000

    change
    c

---
+++ Gameoflife/Gameoflife/Admin/User.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="User.aspx.cs" Inherits="Gameoflife.Admin.WebForm1" %>
+<%@ Register TagPrefix="uc" TagName="MyCustomControl" Src="~/Admin/AdminUserControl.ascx" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <uc:MyCustomControl  runat="server" />
+    <div>
+    <asp:GridView runat="server" ID="grdUsers"></asp:GridView>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Gameoflife/Gameoflife/Admin/User.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Gameoflife.Admin
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            using (var connection = new SqlConnection(Globals.GameOfLifeConnectionString))
+            {
+                connection.Open();
+
+                var command = new SqlCommand("select * from [User]", connection);
+                var adapter = new SqlDataAdapter(command);
+                var dataTable = new DataTable();
+                adapter.Fill(dataTable);
+                grdUsers.DataSource = dataTable;
+                grdUsers.DataBind();
+            }
+        }
+    }
+}
